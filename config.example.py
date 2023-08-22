085ef3ba1438454b91c10e5adc81114fdabe222e
samba <samba@manjaro>
2023-08-22 23:06:29 +0200

    example updated

--- config.example.py
+++ config.example.py
@@ -2,11 +2,13 @@
 OutputDevice = 'SC-8850 Part B'
 InputDevice = 'Launchkey MK2 49 MIDI 1'
 
+#### register the note off events
+TriggerVelocity = 0 # some devices (LPD 8) send 128 note off velocity
 
 # event array [153, 40, 50] => "153.40", no whitespaces
 InputMap = {
-    "128.62": "stop",  #TODO add vel values, for now these are hardcoded
-    "128.64": "start",
-    "144.60": "next",
-    "144.59": "prev"
+    "176.114": "stop", 
+    "176.115": "start",
+    "176.113": "next",
+    "176.112": "prev"
 }
